5666b9c725d2786b9c74f2f1c55e272a9d4e9f01
Professional <mamochapa@gmail.com>
2021-12-07 18:40:03 +0200

    Create LinearLayout

--- app/src/main/java/com/android/example/shoppinglist/data/ShopListRepositoryImpl.kt
+++ app/src/main/java/com/android/example/shoppinglist/data/ShopListRepositoryImpl.kt
@@ -10,10 +10,13 @@ import com.android.example.shoppinglist.domain.ShopListRepository
 object ShopListRepositoryImpl: ShopListRepository {
 
     private val shopListLD = MutableLiveData<List<ShopItem>>()
-    private val shopList = mutableListOf<ShopItem>()
+    //private val shopList = mutableListOf<ShopItem>()
+    //меняем на сортированный список
+    private val shopList = sortedSetOf<ShopItem>({ o1, o2 -> o1.id.compareTo(o2.id)})
+
     private var autoIncrementId = 0
     init{
-        for (i in 0 until 10){
+        for (i in 0 until 10000){
             val item = ShopItem("Name $i", i, true)
             addShopItem(item)
         }
--- app/src/main/java/com/android/example/shoppinglist/presentation/MainActivity.kt
+++ app/src/main/java/com/android/example/shoppinglist/presentation/MainActivity.kt
@@ -3,30 +3,60 @@ package com.android.example.shoppinglist.presentation
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.LayoutInflater
+import android.widget.LinearLayout
+import android.widget.TextView
 import androidx.lifecycle.ViewModelProvider
 import com.android.example.shoppinglist.R
+import com.android.example.shoppinglist.domain.ShopItem
 
 class MainActivity : AppCompatActivity() {
 
     // так как точно будет присваивать значение, поэтому можем использовать lateinit var
     // чтобы не делать постоянную проверку на NULL
     private lateinit var viewModel: MainViewModel
+    private lateinit var llshopList: LinearLayout
 
-    private var count = 0
+//    private var count = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        llshopList = findViewById(R.id.ll_shop_list)
         viewModel = ViewModelProvider(this)[MainViewModel::class.java]      // 1-variant NEW
         //viewModel = ViewModelProvider(this).get(MainViewModel::class.java)       // 2-variant OLD
         viewModel.shopList.observe(this){
-            Log.d("mainActivityTest", it.toString())    // передаем список елементов приведенный к строчному виду
-            if (count == 0) {
-                count++
-                val item = it[0]
-                viewModel.changeEnableState(item)
-            }
+//            Log.d("mainActivityTest", it.toString())    // передаем список елементов приведенный к строчному виду
+//            if (count == 0) {
+//                count++
+//                val item = it[0]
+//                viewModel.changeEnableState(item)
+//            }
+            showList(it)
         }
 //        viewModel.getShopList()
+    }
+
+    private fun showList(list: List<ShopItem>) {
+        llshopList.removeAllViews()
+        for (shopItem in list){
+            val layoutId = if (shopItem.enable){
+                R.layout.item_shop_enabled
+            } else {
+                R.layout.item_shop_disablled
+            }
+            // из макета в layout создаем view
+            // inflate и findViewById очень ресурсозатратные операции, нужно реже выполнять
+            val view = LayoutInflater.from(this).inflate(layoutId, llshopList, false)
+            val tvName = view.findViewById<TextView>(R.id.tv_name)
+            val tvCount = view.findViewById<TextView>(R.id.tv_count)
+            tvName.text = shopItem.name
+            tvCount.text = shopItem.count.toString()
+            view.setOnLongClickListener{
+                viewModel.changeEnableState(shopItem)
+                true
+            }
+            llshopList.addView(view)
+        }
     }
 }
