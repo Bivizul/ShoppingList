d0c24d5a072f13a6ea1cb44a7eb83205e7afb5ac
Professional <mamochapa@gmail.com>
2021-12-07 14:21:46 +0200

    Create presentation layer

--- app/src/main/java/com/android/example/shoppinglist/presentation/MainActivity.kt
+++ app/src/main/java/com/android/example/shoppinglist/presentation/MainActivity.kt
@@ -2,11 +2,24 @@ package com.android.example.shoppinglist.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.lifecycle.ViewModelProvider
 import com.android.example.shoppinglist.R
 
 class MainActivity : AppCompatActivity() {
+
+    // так как точно будет присваивать значение, поэтому можем использовать lateinit var
+    // чтобы не делать постоянную проверку на NULL
+    private lateinit var viewModel: MainViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        viewModel = ViewModelProvider(this)[MainViewModel::class.java]      // 1-variant NEW
+        //viewModel = ViewModelProvider(this).get(MainViewModel::class.java)       // 2-variant OLD
+        viewModel.shopList.observe(this){
+            Log.d("mainActivityTest", it.toString())    // передаем список елементов приведенный к строчному виду
+        }
+        viewModel.getShopList()
     }
 }
---
+++ app/src/main/java/com/android/example/shoppinglist/presentation/MainViewModel.kt
@@ -0,0 +1,44 @@
+package com.android.example.shoppinglist.presentation
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.android.example.shoppinglist.data.ShopListRepositoryImpl
+import com.android.example.shoppinglist.domain.DeleteShopItemUseCase
+import com.android.example.shoppinglist.domain.EditShopItemUseCase
+import com.android.example.shoppinglist.domain.GetShopListUseCase
+import com.android.example.shoppinglist.domain.ShopItem
+
+// : AndroidViewModel() наследование нужно,если во ViewModel есть контекст
+// Если контекста нет то можно наследоваться от : ViewModel , в его конструктор ничего не нужно передавать
+// presentation и data слой недолжны знать друг о друге НИЧЕГО
+class MainViewModel: ViewModel() {
+
+    private val repository = ShopListRepositoryImpl
+
+    private val getShopListUseCase = GetShopListUseCase(repository)
+    private val deleteShopItemUseCase = DeleteShopItemUseCase(repository)
+    private val editShopItemUseCase = EditShopItemUseCase(repository)
+
+    // MutableLiveData - это LiveData в которую мы сами можем вставлять объекты
+    val shopList = MutableLiveData<List<ShopItem>>()
+
+    fun getShopList(){
+        val list = getShopListUseCase.getShopList()
+        // добавляем елемент
+        shopList.value = list      // можно вызывать только из главного потока
+        //shopList.postValue = list       // можно вызывать из любого потока
+    }
+
+    fun deleteShopItem(shopItem: ShopItem){
+        deleteShopItemUseCase.deleteShopItem(shopItem)
+        getShopList()
+    }
+
+    fun changeEnableState(shopItem: ShopItem){
+        val newItem = shopItem.copy(enable = !shopItem.enable)
+        editShopItemUseCase.editShopItem(newItem)
+        getShopList()
+
+    }
+}
