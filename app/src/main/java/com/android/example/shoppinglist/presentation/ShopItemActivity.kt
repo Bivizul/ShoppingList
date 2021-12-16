f4c371faf94a1f89261f2adca8d16203c57aff8c
Professional <mamochapa@gmail.com>
2021-12-16 17:12:16 +0200

    Finish work above ShopItemActivity

--- app/src/main/java/com/android/example/shoppinglist/presentation/ShopItemActivity.kt
+++ app/src/main/java/com/android/example/shoppinglist/presentation/ShopItemActivity.kt
@@ -2,21 +2,159 @@ package com.android.example.shoppinglist.presentation
 
 import android.content.Context
 import android.content.Intent
-import android.content.LocusId
 import android.os.Bundle
-import android.util.Log
+import android.text.Editable
+import android.text.TextWatcher
+import android.widget.Button
+import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
 import com.android.example.shoppinglist.R
+import com.android.example.shoppinglist.domain.ShopItem
+import com.google.android.material.textfield.TextInputLayout
 
 // данный экран может работать в режиме добавления и в режиме редактирования
 class ShopItemActivity : AppCompatActivity() {
+
+    // ссылка на viewModel
+    private lateinit var viewModel: ShopItemViewModel
+
+    private lateinit var tilName: TextInputLayout
+    private lateinit var tilCount: TextInputLayout
+    private lateinit var etName: EditText
+    private lateinit var etCount: EditText
+    private lateinit var buttonSave: Button
+
+    private var screenMode = MODE_UNKNOWN
+    private var shopItemId = ShopItem.UNDEFINED_ID
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        // устанавливаем контент
         setContentView(R.layout.activity_shop_item)
-        // получим переданные значения
+        // проверяем все ли параметры передаются intent-u
+        parseIntent()
+        // инициализируем ссылку на viewModel
+        viewModel = ViewModelProvider(this)[ShopItemViewModel::class.java]
+        // вызовим метод инициализации, все view элементы
+        initViews()
+        // добавляем слушателей ввода текста
+        addTextChangeListener()
+        // запускаем правильный режим экрана
+        launchRightMode()
+        // подписываемся на все объекты viewModel
+        observeViewModel()
+
+    }
+
+    // подпишемся на остальные объекты из viewModel
+    private fun observeViewModel() {
+        viewModel.errorInputCount.observe(this) {
+            val message = if (it) {
+                getString(R.string.error_input_count)
+            } else {
+                null
+            }
+            tilCount.error = message
+        }
+        viewModel.errorInputName.observe(this) {
+            val message = if (it) {
+                getString(R.string.error_input_name)
+            } else {
+                null
+            }
+            tilCount.error = message
+        }
+        // если работа с экраном завершена, то закроем экран
+        viewModel.shouldCloseScreen.observe(this) {
+            finish()
+        }
+    }
+
+    private fun launchRightMode() {
+        // настраиваем экран
+        when (screenMode) {
+            MODE_EDIT -> launchEditMode()
+            MODE_ADD -> launchAddMode()
+        }
+    }
+
+    // устанавливаем слушателя текста
+    private fun addTextChangeListener() {
+        etName.addTextChangedListener(object : TextWatcher {
+            // скрываем ошибку при вводе текста
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                viewModel.resetErrorInputName()
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+
+            }
+        })
+        etCount.addTextChangedListener(object : TextWatcher {
+            // скрываем ошибку при вводе текста
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                viewModel.resetErrorInputCount()
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+
+            }
+        })
+    }
+
+    private fun launchEditMode() {
+        // получаем элемент по id
+        viewModel.getShopItem(shopItemId)
+        // подписываемся на данный элемент
+        viewModel.shopItem.observe(this) {
+            etName.setText(it.name)
+            etCount.setText(it.count.toString())
+        }
+        buttonSave.setOnClickListener {
+            viewModel.editShopItem(etName.text?.toString(), etCount.text?.toString())
+        }
+    }
+
+    private fun launchAddMode() {
+        buttonSave.setOnClickListener {
+            viewModel.addShopItem(etName.text?.toString(), etCount.text?.toString())
+        }
+    }
+
+    private fun parseIntent() {
+        // проверим сожерание intent(намерения), при null кинем исключение
+        if (!intent.hasExtra(EXTRA_SCREEN_MODE)) {
+            throw RuntimeException("Param screen mode is absent")
+        }
         val mode = intent.getStringExtra(EXTRA_SCREEN_MODE)
-        // выводим лог, просто mode передать нельзя, так как он NULL, приложение упадет
-        Log.d("ShopItemActivity", mode.toString())
+        if (mode != MODE_EDIT && mode != MODE_ADD) {
+            throw RuntimeException("Unknown screen mode $mode")
+        }
+        screenMode = mode
+        if (screenMode == MODE_EDIT) {
+            if (!intent.hasExtra(EXTRA_SHOP_ITEM_ID)) {
+                throw RuntimeException("Param shop item id is absent")
+            }
+            shopItemId = intent.getIntExtra(EXTRA_SHOP_ITEM_ID, ShopItem.UNDEFINED_ID)
+        }
+    }
+
+    // инициализация всех VIEW
+    private fun initViews() {
+        tilName = findViewById(R.id.til_name)
+        tilCount = findViewById(R.id.til_count)
+        etName = findViewById(R.id.et_name)
+        etCount = findViewById(R.id.et_count)
+        buttonSave = findViewById(R.id.save_button)
     }
 
     companion object {
@@ -25,6 +163,8 @@ class ShopItemActivity : AppCompatActivity() {
         private const val EXTRA_SHOP_ITEM_ID = "extra_shop_item_id"
         private const val MODE_EDIT = "mode_edit"
         private const val MODE_ADD = "mode_add"
+        private const val MODE_UNKNOWN = ""
+
 
         // хорошая практика использовать статические методы указанные ниже
 
@@ -36,7 +176,7 @@ class ShopItemActivity : AppCompatActivity() {
         }
 
         // запускаем экран в режимае редактирования
-        fun newIntentEditItem(context: Context, shopItemId: Int): Intent{
+        fun newIntentEditItem(context: Context, shopItemId: Int): Intent {
             val intent = Intent(context, ShopItemActivity::class.java)
             intent.putExtra(EXTRA_SCREEN_MODE, MODE_EDIT)
             intent.putExtra(EXTRA_SHOP_ITEM_ID, shopItemId)
